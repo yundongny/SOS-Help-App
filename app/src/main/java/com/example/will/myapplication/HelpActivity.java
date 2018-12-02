cb3102030ac10ed4021a5a5086fddbe59595c75c
willjg <wjguo2@illinois.edu>
2018-12-02 14:24:33 -0600

    Home Screen Layout

--- app/src/main/java/com/example/will/myapplication/HelpActivity.java
+++ app/src/main/java/com/example/will/myapplication/HelpActivity.java
@@ -2,14 +2,12 @@ package com.example.will.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button goToHelp = findViewById(R.id.button);
+        setContentView(R.layout.activity_help);
     }
 }
