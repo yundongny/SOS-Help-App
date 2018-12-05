656b82ab7d52c064d7c8a41716e5a677b130edb8
Justin <yundongny717@gmail.com>
2018-12-05 00:36:00 -0600

    created a new object for studygroupitem

--- app/src/main/java/com/example/will/myapplication/HelpActivity.java
+++ app/src/main/java/com/example/will/myapplication/HelpActivity.java
@@ -8,6 +8,6 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+        setContentView(R.layout.activity_get_help);
     }
 }
