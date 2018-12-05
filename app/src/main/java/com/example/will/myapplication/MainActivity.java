47d55a6a597cd28a438086e53f606fb92c71043e
willjg <wjguo2@illinois.edu>
2018-12-05 15:02:48 -0600

    Linked up pages, will need to debug

--- app/src/main/java/com/example/will/myapplication/MainActivity.java
+++ app/src/main/java/com/example/will/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.will.myapplication;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        final Button GetHelp = findViewById(R.id.getHelpBtn);
+        GetHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), JoinGroup.class);
+                startActivity(intent);
+            }
+        });
+        final Button GiveHelp = findViewById(R.id.giveHelpBtn);
+        GiveHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), createGroup.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
