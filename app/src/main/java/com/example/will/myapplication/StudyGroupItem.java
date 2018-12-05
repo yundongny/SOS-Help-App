0edce5a4136f63ffbfd0a207d9b90d55b28e0f98
Justin <yundongny717@gmail.com>
2018-12-04 23:57:15 -0600

    created a new object for studygroupitem

---
+++ app/src/main/java/com/example/will/myapplication/StudyGroupItem.java
@@ -0,0 +1,47 @@
+package com.example.will.myapplication;
+
+public class StudyGroupItem {
+    private String name;
+    private String time;
+    private String subject;
+    private int numberOfPeople;
+
+    public StudyGroupItem() {
+        this.name = "";
+        this.time = "";
+        this.subject = "";
+        this.numberOfPeople = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+}
