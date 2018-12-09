726d10d2bf2050ef93681a5010df54a54a789f99
willjg <wjguo2@illinois.edu>
2018-12-08 19:57:54 -0600

    Made changes to Main Activity and Profile Page
    Linking up with Firebase

--- app/src/main/java/com/example/will/myapplication/MainActivity.java
+++ app/src/main/java/com/example/will/myapplication/MainActivity.java
@@ -8,10 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        final int RC_SIGN_IN = 123;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageButton ProfilePicture = findViewById(R.id.ProfilePicture);
@@ -39,6 +53,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
 
 
--- app/src/main/java/com/example/will/myapplication/ProfilePage.java
+++ app/src/main/java/com/example/will/myapplication/ProfilePage.java
@@ -1,11 +1,16 @@
 package com.example.will.myapplication;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfilePage extends AppCompatActivity {
 
@@ -13,17 +18,27 @@ public class ProfilePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView nameTxt = findViewById(R.id.Username);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            nameTxt.setText(name);
+            String email = user.getEmail();
+
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+
     }
+
 
 }
